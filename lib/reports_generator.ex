55e4fcac79acf523040731545b9bb9d8f8507f58
brenoos <breno.oliver07@gmail.com>
2021-03-09 13:37:56 -0300

    finishing report

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -5,12 +5,14 @@ defmodule ReportsGenerator do
     "açaí",
     "churrasco",
     "esfirra",
-    "hambúrger",
+    "hambúrguer",
     "pastel",
     "pizza",
     "prato_feito",
     "sushi"
   ]
+
+  @options ["foods", "users"]
 
   def build(filename) do
     filename
@@ -20,9 +22,11 @@ defmodule ReportsGenerator do
     end)
   end
 
-  def fetch_higher_cost(report) do
-    Enum.max_by(report, fn {_key, value} -> value end)
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option!"}
 
   defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
     users = Map.put(users, id, users[id] + price)
@@ -34,7 +38,6 @@ defmodule ReportsGenerator do
   defp report_acc do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
-
     %{"users" => users, "foods" => foods}
   end
 end
