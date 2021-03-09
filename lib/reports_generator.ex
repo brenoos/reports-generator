93fa9a09791fe45ed286d4a9d51ac5a6f0d0dabd
brenoos <breno.oliver07@gmail.com>
2021-03-09 13:07:58 -0300

    higher cost

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -4,7 +4,13 @@ defmodule ReportsGenerator do
   def build(filename) do
     filename
     |> Parser.parse_file()
-    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+    |> Enum.reduce(report_acc(), fn line, report ->
+      sum_values(line, report)
+    end)
+  end
+
+  def fetch_higher_cost(report) do
+    Enum.max_by(report, fn {_key, value} -> value end)
   end
 
   defp sum_values([id, _food_name, price], report) do
